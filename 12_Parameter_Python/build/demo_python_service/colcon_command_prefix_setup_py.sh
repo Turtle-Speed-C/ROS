7686a2220011290360112a865b7f666981831a69
Turtle-Speed-C <2423969470@qq.com>
2025-12-04 13:37:13 +0800

    12.4提交

---
+++ 12_Parameter_Python/build/demo_python_service/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/lxf/Desktop/ROS/12_Parameter_Python/install/face_interfaces/share/face_interfaces/package.sh"
---
+++ 12_Parameter_Python/build/face_interfaces/CMakeFiles/face_interfaces__rosidl_typesupport_fastrtps_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face_interfaces__rosidl_typesupport_fastrtps_cpp.dir/rosidl_typesupport_fastrtps_cpp/face_interfaces/srv/detail/dds_fastrtps/face_detector__type_support.cpp.o"
+  "CMakeFiles/face_interfaces__rosidl_typesupport_fastrtps_cpp.dir/rosidl_typesupport_fastrtps_cpp/face_interfaces/srv/detail/dds_fastrtps/face_detector__type_support.cpp.o.d"
+  "libface_interfaces__rosidl_typesupport_fastrtps_cpp.pdb"
+  "libface_interfaces__rosidl_typesupport_fastrtps_cpp.so"
+  "rosidl_typesupport_fastrtps_cpp/face_interfaces/srv/detail/dds_fastrtps/face_detector__type_support.cpp"
+  "rosidl_typesupport_fastrtps_cpp/face_interfaces/srv/detail/face_detector__rosidl_typesupport_fastrtps_cpp.hpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/face_interfaces__rosidl_typesupport_fastrtps_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
